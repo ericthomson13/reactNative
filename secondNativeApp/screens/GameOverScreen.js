0ce3a202e5a72c29bdbc83b8ad415860a80343ac
et <eric.thomson13@gmail.com>
2020-03-20 12:02:07 -0600

    Styled Image for GameOver

--- secondNativeApp/screens/GameOverScreen.js
+++ secondNativeApp/screens/GameOverScreen.js
@@ -8,7 +8,13 @@ const GameOverScreen = ({ roundsNumber, userNumber, onRestart }) => {
   return (
     <View style={styles.screen}>
       <TitleText>The Game is Over!</TitleText>
-      <Image source={require('../assets/success.png')} />
+      <View style={styles.imageContainer}>
+        <Image 
+          source={require('../assets/success.png')} 
+          style={styles.image}
+          resizeMethod="scale"
+        />
+      </View>
       <BodyText>Number of rounds: {roundsNumber}</BodyText>
       <BodyText>Number was: {userNumber}</BodyText>
       <Button title="NEW GAME" onPress={onRestart}/>
@@ -21,7 +27,20 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  }
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  imageContainer: {
+    borderRadius: 200,
+    borderWidth: 3,
+    borderColor: 'black',
+    width: '80%',
+    height: '80%',
+    overflow: 'hidden',
+    marginVertical: 30, 
+  },
 });
 
 export default GameOverScreen;
