882b157416d174793e2b8fd3d5d075f6c638111e
et <eric.thomson13@gmail.com>
2020-03-20 10:19:33 -0600

    Added Custom Font Files

--- secondNativeApp/App.js
+++ secondNativeApp/App.js
@@ -1,5 +1,6 @@
 import React, { useState, } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
 
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
