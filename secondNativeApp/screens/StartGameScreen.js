30e9b336e137e2aa68f40ea3fdec7cf79a4a3656
et <eric.thomson13@gmail.com>
2020-03-23 10:32:21 -0600

    StartGame Updated for Landscape View

--- secondNativeApp/screens/StartGameScreen.js
+++ secondNativeApp/screens/StartGameScreen.js
@@ -7,6 +7,8 @@ import {
   Keyboard,
   Alert,
   Dimensions,
+  ScrollView,
+  KeyboardAvoidingView,
 } from 'react-native';
 
 import Card from '../components/Card';
@@ -58,34 +60,38 @@ const StartGameScreen = (props) => {
   }
 
   return (
-    <TouchableWithoutFeedback onPress={() => {Keyboard.dismiss()}}>
-      <View style={styles.screen}>
-        <TitleText styles={styles.title}>Start a New Game</TitleText>
-        <Card style={styles.inputContainer}>
-          <BodyText style={styles.text}>Select a Number</BodyText>
-            <Input
-              style={styles.input} 
-              blurOnSubmit
-              autoCapitalize='none' 
-              // below is getting passed into props as string !Boolean throwing Err
-              // autoCorrect={false}
-              keyboardType='number-pad' 
-              maxLength={2} 
-              onChangeText={numberInputHandler}
-              value={enteredValue}
-            />       
-            <View style={styles.buttonContainer}>
-            <View style={styles.button}>
-              <Button title="Reset" color={Colors.accent} onPress={resetInputHandler} />
+    <ScrollView>
+      <KeyboardAvoidingView behavior="position" keyboardVerticalOffset={30} >
+      <TouchableWithoutFeedback onPress={() => {Keyboard.dismiss()}}>
+        <View style={styles.screen}>
+          <TitleText styles={styles.title}>Start a New Game</TitleText>
+          <Card style={styles.inputContainer}>
+            <BodyText style={styles.text}>Select a Number</BodyText>
+              <Input
+                style={styles.input} 
+                blurOnSubmit
+                autoCapitalize='none' 
+                // below is getting passed into props as string !Boolean throwing Err
+                // autoCorrect={false}
+                keyboardType='number-pad' 
+                maxLength={2} 
+                onChangeText={numberInputHandler}
+                value={enteredValue}
+              />       
+              <View style={styles.buttonContainer}>
+              <View style={styles.button}>
+                <Button title="Reset" color={Colors.accent} onPress={resetInputHandler} />
+              </View>
+              <View style={styles.button} >
+                <Button title="Confirm" color={Colors.primary} onPress={confirmInputHandler}/>
+              </View>
             </View>
-            <View style={styles.button} >
-              <Button title="Confirm" color={Colors.primary} onPress={confirmInputHandler}/>
-            </View>
-          </View>
-        </Card>
-        {confirmedOutput}
-      </View>
-    </TouchableWithoutFeedback>
+          </Card>
+          {confirmedOutput}
+        </View>
+      </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    </ScrollView>
   );
 };
 
@@ -110,7 +116,7 @@ const styles = StyleSheet.create({
     width: '100%',
     justifyContent: 'space-between',
     paddingHorizontal: 15,
-    
+
   },
   button: {
     width: Dimensions.get('window').width / 4,
