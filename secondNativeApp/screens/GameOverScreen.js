f92e825404d9eca74b9f2d672decf07e947e5aa2
et <eric.thomson13@gmail.com>
2020-03-23 10:22:13 -0600

    GameOver Updated for Smaller Screens

--- secondNativeApp/screens/GameOverScreen.js
+++ secondNativeApp/screens/GameOverScreen.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import { View, StyleSheet, Image, Text, } from 'react-native';
+import { 
+  View,
+  StyleSheet,
+  Image,
+  Text,
+  Dimensions,
+  ScrollView,
+} from 'react-native';
 
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText';
@@ -9,23 +16,25 @@ import Colors from '../constants/colors';
 
 const GameOverScreen = ({ roundsNumber, userNumber, onRestart }) => {
   return (
-    <View style={styles.screen}>
-      <TitleText>The Game is Over!</TitleText>
-      <View style={styles.imageContainer}>
-        <Image 
-          source={require('../assets/success.png')} 
-          style={styles.image}
-          resizeMethod="scale"
-        />
+    <ScrollView>
+      <View style={styles.screen}>
+        <TitleText>The Game is Over!</TitleText>
+        <View style={styles.imageContainer}>
+          <Image 
+            source={require('../assets/success.png')} 
+            style={styles.image}
+            resizeMethod="scale"
+          />
+        </View>
+        <View style={styles.resultContainer}>
+          <BodyText style={styles.resultText}>
+            Your phone needed <Text style={styles.highlight}>{roundsNumber}</Text> rounds to guess the number <Text style={styles.highlight}>{userNumber}</Text>.
+          </BodyText>
+        </View>
+        
+        <MainButton onPress={onRestart}>NEW GAME</MainButton>
       </View>
-      <View style={styles.resultContainer}>
-        <BodyText style={styles.resultText}>
-          Your phone needed <Text style={styles.highlight}>{roundsNumber}</Text> rounds to guess the number <Text style={styles.highlight}>{userNumber}</Text>.
-        </BodyText>
-      </View>
-      
-      <MainButton onPress={onRestart}>NEW GAME</MainButton>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -40,13 +49,13 @@ const styles = StyleSheet.create({
     height: '100%',
   },
   imageContainer: {
-    borderRadius: 200,
+    borderRadius: Dimensions.get('window').width * 0.35,
     borderWidth: 3,
     borderColor: 'black',
-    width: '80%',
-    height: '80%',
+    width: Dimensions.get('window').width * 0.7,
+    height: Dimensions.get('window').width * 0.7,
     overflow: 'hidden',
-    marginVertical: 30, 
+    marginVertical: Dimensions.get('window').height / 30, 
   },
   highlight: {
     color: Colors.primary,
@@ -54,11 +63,11 @@ const styles = StyleSheet.create({
   },
   resultContainer: {
     marginHorizontal: 30,
-    marginVertical: 15,
+    marginVertical: Dimensions.get('window').height / 60,
   },
   resultText: {
     textAlign: 'center',
-    fontSize: 20,
+    fontSize: Dimensions.get('window').height < 400 ? 16 : 20,
   },
 });
 
