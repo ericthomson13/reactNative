17deded76e7e41ef1a30da7b0877a26a8201756d
et <eric.thomson13@gmail.com>
2020-03-25 15:39:48 -0600

    renderGridItems Added into Categories

--- mealsApp/screens/CategoriesScreen.js
+++ mealsApp/screens/CategoriesScreen.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button, FlatList, } from 'react-native';
 
+import { CATEGORIES } from '../data/dummy-data';
+
 const CategoriesScreen = ({ navigation, }) => {
+  const renderGridItem = (itemData) => {
+    return (
+      <View style={styles.gridItem} >
+        <Text>
+          {itemData.item.title}
+        </Text>
+      </View>
+    );
+  };
 
   return (
-    <FlatList numColumns={2} />
+    <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
   )
 };
 
@@ -12,9 +23,14 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center', 
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
     
-  }
+  },
 });
 
 export default CategoriesScreen;
