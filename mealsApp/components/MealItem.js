269b02555e45b15da7c40c3a493dda82531759d4
et <eric.thomson13@gmail.com>
2020-03-26 11:29:19 -0600

    Basic Function and Styling for MealItem

--- mealsApp/components/MealItem.js
+++ mealsApp/components/MealItem.js
@@ -14,11 +14,13 @@ const MealItem = ({
     <View style={styles.mealItem}>
       <TouchableOpacity onPress={onSelectMeal}>
         <View>
+        <ImageBackground source={{ uri: imageUrl, }}>
           <View style={{ ...styles.mealRow, ...styles.mealHeader }}>
-            <ImageBackground source={{ uri: imageUrl, }}>
-              <Text>{title}</Text> 
-            </ImageBackground>
+              <View style={styles.titleContainer} >
+                <Text style={styles.title} numberOfLines={1} >{title}</Text> 
+              </View>
           </View>
+        </ImageBackground>
           <View style={{ ...styles.mealRow, ...styles.mealDetail }}>
             <Text>{duration}</Text>
             <Text>{complexity.toUpperCase()}</Text>
@@ -34,21 +36,37 @@ const styles = StyleSheet.create({
   mealItem: {
     height: 200,
     width: '100%',
-    backgroundColor: '#ccc',
+    backgroundColor: '#f5f5f5',
+    borderRadius: 10,
+    overflow: 'hidden',
   },
   bgImage: {
     width: '100%',
     height: '100%',
+    justifyContent: 'flex-end',
   },
   mealRow: {
     flexDirection: 'row',
   },
   mealHeader: {
-    height: '90%',
+    height: '85%',
   },
   mealDetail: {
     paddingHorizontal: 10,
     justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '15%',
+  },
+  titleContainer: {
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    paddingVertical: 5,
+  },
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 22,
+    color: 'white',
+    paddingHorizontal: 12,
+    textAlign: 'center',
   },
 });
 export default MealItem;
