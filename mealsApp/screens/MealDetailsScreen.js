0be9f9f255c11d92f0f57cc2c03a28a337b88b06
et <eric.thomson13@gmail.com>
2020-03-27 11:02:11 -0600

    MealDetails Updated

--- mealsApp/screens/MealDetailsScreen.js
+++ mealsApp/screens/MealDetailsScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button, } from 'react-native';
+import { View, Text, StyleSheet, Button, ScrollView, Image, } from 'react-native';
 import { HeaderButtons, Item, } from 'react-navigation-header-buttons';
 
 import CustomHeaderButton from '../components/CustomHeaderButton';
+import DefaultText from '../components/DefaultText';
 
 import { MEALS, } from '../data/dummy-data';
 
@@ -11,13 +12,19 @@ const MealDetailsScreen = ({ navigation, }) => {
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
   return (
-    <View style={styles.screen} >
-      <Text>{selectedMeal.title}</Text>
-      <Button 
-        title="Go Back to Categories"
-        onPress={() => navigation.popToTop()}
-      />
-    </View>
+    <ScrollView>
+      <Image source={{ uri: selectedMeal.imageUrl}} style={styles.image} />
+      <View style={styles.details} >
+        <DefaultText>{selectedMeal.duration}</DefaultText>
+        <DefaultText>{selectedMeal.complexity.toUpperCase()}</DefaultText>
+        <DefaultText>{selectedMeal.affordability.toUpperCase()}</DefaultText>
+      </View>
+      <Text style={styles.title}>Ingredients</Text>
+      <Text>List of Ingredients</Text>
+      <Text style={styles.title}>Steps</Text>
+      <Text>List of Steps</Text>
+    </ScrollView>
+    
   )
 };
 
@@ -35,11 +42,15 @@ MealDetailsScreen.navigationOptions = (navigationData) => {
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
+  image: {
+    width: '100%',
+    height: 200,
+  },
+  details: {
+    flexDirection: 'row',
+    padding: 15,
+    justifyContent: 'space-around',
+  },
 });
 
 export default MealDetailsScreen;
