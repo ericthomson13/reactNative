1f3623b42cd0c6e90dcc472d98b5d3c551df307a
et <eric.thomson13@gmail.com>
2020-03-27 10:50:06 -0600

    Added Android Nav Styling

--- mealsApp/navigation/MealsNavigator.js
+++ mealsApp/navigation/MealsNavigator.js
@@ -5,7 +5,7 @@ import { createBottomTabNavigator, } from 'react-navigation-tabs';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import { Ionicons } from '@expo/vector-icons';
 import { createMaterialBottomTabNavigator, } from 'react-navigation-material-bottom-tabs';
-import { Platform, } from 'react-native';
+import { Platform, Text, } from 'react-native';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
@@ -19,6 +19,12 @@ const defaultStackNavOptions = {
   defaultNavigationOptions: {
     headerStyle: {
       backgroundColor: Platform.OS === 'android' || 'web' ? colors.primaryColor : '',
+    },
+    headerTitleStyle: {
+      fontFamily: 'open-sans-bold',
+    },
+    headerBackTitleStyle: {
+      fontFamily: 'open-sans',
     },
     headerTintColor: Platform.OS === 'android' || 'web' ? 'white' : colors.primaryColor,
   }
@@ -47,6 +53,7 @@ const tabScreenConfig = {
         return <Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />
       },
       tabBarColor: colors.primaryColor,
+      tabBarLabel: Platform.OS === 'android' ? <Text style={{ fontFamily: 'open-sans-bold' }}>Meals</Text> : 'Meals',
     }
   },
   Favorites: {
@@ -56,6 +63,7 @@ const tabScreenConfig = {
         return <Ionicons name="ios-star" size={25} color={tabInfo.tintColor}/>
       },
       tabBarColor: colors.accentColor,
+      tabBarLabel: Platform.OS === 'android' ? <Text style={{ fontFamily: 'open-sans-bold' }}>Meals</Text> : 'Meals',
     }
   },
 };
@@ -67,6 +75,9 @@ const MealsFavTabNavigator = Platform.OS === 'android' ? createMaterialBottomTab
 : createBottomTabNavigator(tabScreenConfig, {
   tabBarOptions: {
     activeTintColor: colors.accentColor,
+    labelStyle: {
+      fontFamily: 'open-sans-bold',
+    },
   }
 });
 
