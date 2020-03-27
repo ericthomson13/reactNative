f0f76a2a66f7cb1eb21da6425331aafcde5fe3d1
et <eric.thomson13@gmail.com>
2020-03-27 16:36:15 -0600

    MealsApp Wrapped Up

--- mealsApp/screens/CategoryMealsScreen.js
+++ mealsApp/screens/CategoryMealsScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import { View, StyleSheet, } from 'react-native';
 import { useSelector, } from 'react-redux';
 
 import MealList from '../components/MealList';
+import DefaultText from '../components/DefaultText';
 
 import { CATEGORIES, } from '../data/dummy-data';
 
@@ -10,7 +11,13 @@ const CategoriesMealsScreen = ({ navigation, }) => {
   const catId = navigation.getParam('categoryId');
   const availableMeals = useSelector((state) => state.meals.filteredMeals)
   const displayedMeals = availableMeals.filter((meal) => meal.categoryIds.indexOf(catId) >= 0 );
-
+  if (displayedMeals.length === 0 || !displayedMeals) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No meals found, maybe you should check filters?</DefaultText>
+      </View>
+    );
+  }
   return (
    <MealList listData={displayedMeals} navigation={navigation} />
   );
@@ -24,6 +31,12 @@ CategoriesMealsScreen.navigationOptions = (navigationData) => {
   };
 };
 
-
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    alignContent: 'center',
+    justifyContent: 'center',
+  }
+});
 
 export default CategoriesMealsScreen;
