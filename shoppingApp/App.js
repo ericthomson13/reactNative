77175118d479aa59d569336b6432aee140dc1c3b
et <eric.thomson13@gmail.com>
2020-03-28 12:06:37 -0600

    Navigator Added

--- shoppingApp/App.js
+++ shoppingApp/App.js
@@ -4,7 +4,7 @@ import { combineReducers, createStore, } from 'redux';
 import { Provider, } from 'react-redux';
 
 import productsReducer from './store/reducers/products';
-import { createStackNavigator } from 'react-navigation-stack';
+import ShopNavigator from './navigation/ShopNavigator';
 
 const rootReducer = combineReducers({
   products: productsReducer,
@@ -15,9 +15,7 @@ const store = createStore(rootReducer);
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <ShopNavigator />
     </Provider>
   );
 }
---
+++ shoppingApp/navigation/ShopNavigator.js
@@ -0,0 +1,20 @@
+import { createStackNavigator, } from 'react-navigation-stack';
+import { Platform, } from 'react-native';
+import { createAppContainer, } from 'react-navigation';
+
+import ProductsOverviewScreen from '../screens/shop/ProductOverviewScreen';
+
+import Colors from '../constants/Colors';
+
+const ProductsNavigator = createStackNavigator({
+  ProductsOverview: ProductsOverviewScreen,
+}, {
+  defaultNavigationOptions: {
+    headerStyle: {
+      backgroundColor: Platform.OS !== 'ios' ? Colors.primary : '',
+    },
+    headerTintColor: Platform.OS !== 'ios' ? 'white' : Colors.primary,
+  }
+});
+
+export default createAppContainer(ProductsNavigator);
